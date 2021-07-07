b29f1e0cc34a74e5477650fa78a10b077c382efe
Shen-En Shih <petershih@google.com>
2021-07-07 09:56:39 -0700

    start minicap in script

--- device/minicap_client.py
+++ device/minicap_client.py
@@ -40,10 +40,12 @@ def read_frames(socket):
 class MinicapServer(QRunnable):
     def run(self) -> None:
         print("Killing old minicap server")
-        subprocess.run(["killall", "minicap"])
+        subprocess.call("killall minicap", shell=True)
+        #subprocess.run(["killall", "minicap"], shell=True)
 
         print("Starting Minicap server")
-        subprocess.run(["/usr/local/bin/adb", "shell", "LD_LIBRARY_PATH=/data/local/tmp", " /data/local/tmp/minicap", "-P", "2340x1080@2340x1080/0"])
+        #subprocess.run(["/usr/local/bin/adb", "shell", "LD_LIBRARY_PATH=/data/local/tmp", " /data/local/tmp/minicap", "-P", "2340x1080@2340x1080/0"], shell=True)
+        subprocess.call("adb shell LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/minicap -P 2340x1080@2340x1080/0", shell=True)
 
         print("Minicap server died.")
 
@@ -64,7 +66,7 @@ class MinicapClient(QRunnable):
         print("adb shell killall minicap")
         print("adb shell LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/minicap -P 2340x1080@2340x1080/0")
         print("adb forward tcp:1313 localabstract:minicap")
-        #self.__connect_minicap()
+        self.__connect_minicap()
 
         self.__connection = socket.create_connection(('127.0.0.1', 1313))
 
@@ -92,11 +94,13 @@ class MinicapClient(QRunnable):
 
     def __forward_minicap_port(self):
         print("kill existing forward")
-        subprocess.run(["/usr/local/bin/adb", "forward", "--remove-all"])
+        #subprocess.run(["/usr/local/bin/adb", "forward", "--remove-all"], shell=True)
+        subprocess.call("adb forward --remove-all", shell=True)
         time.sleep(1)
 
         print("starting forward")
-        subprocess.run(["/usr/local/bin/adb", "forward", "tcp:1313", " localabstract:minicap"])
+        #subprocess.run(["/usr/local/bin/adb", "forward", "tcp:1313", " localabstract:minicap"], shell=True)
+        subprocess.call("adb forward tcp:1313 localabstract:minicap", shell=True)
         print("forward command exited (it's normal...)")
         time.sleep(1)
 
